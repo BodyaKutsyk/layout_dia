a4f5db86689396e603ca005c56da06d7ddeddb93
BodyaKuts <mkkbodya@gmail.com>
2023-08-20 16:56:15 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-document.getElementById('formId').addEventListener('submit', function(event) {
-  event.preventDefault();
-  document.getElementById('inputId-name').value = '';
-  document.getElementById('inputId-email').value = '';
-  document.getElementById('inputId-message').value = '';
-});
